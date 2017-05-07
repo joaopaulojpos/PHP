0b0244717696ee4e0210a09adfc156eaab29f9fd
joaopaulojpos <jpo.oliveiras@gmail.com>
2017-05-07 12:53:54 -0300

    Sistema de controle de usuarios.
    Concluido um pequeno sistema de controle de usuarios.

--- phpdozero/control_user/adicionar.php
+++ phpdozero/control_user/adicionar.php
@@ -14,10 +14,11 @@ if(isset($_POST['nome']) && empty($_POST['nome']) == false)
 	}
 ?>
 
+<form method="POST">
 	Nome:<br/>
 	<input type="text" name="nome"/><br/><br/>
 	E-mail:<br/>
-	<input type="text" name="email"/><br/><br/>
+	<input type="email" name="email"/><br/><br/>
 	Senha:<br/>
 	<input type="password" name="senha"/><br/><br/>
 	<input type="submit" name="Cadastrar"/>
